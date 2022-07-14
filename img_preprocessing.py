63bbbfb50a0b12961ddd490c0ae10efcb0c3ac6b
Siarencjusz <s.s@s>
2022-07-14 18:55:58 +0200

    Implement haiku cnn model training using jax

--- img_preprocessing.py
+++ img_preprocessing.py
@@ -1,9 +1,8 @@
 """Example of an image preprocessing for preparation of a training dataset"""
+import jax
 import jax.numpy as jnp
 import pymongo
 from tqdm import tqdm
-from jax import random
-from jax import image as jimage
 from pymongo import MongoClient
 import dm_pix as pix
 from utils import get_secret, load_image, serialize_jarray_for_mongo
@@ -19,14 +18,14 @@ def preprocess_imgs(input_imgs: pymongo.collection, img_names: list) -> jnp.ndar
     preprocessed_imgs = []
     for img_name in tqdm(img_names, unit='img', desc='Preprocessing img'):
         org_img = load_image(input_imgs.find_one({'_id': img_name})['content'])
-        img = jimage.resize(org_img, jnp.array(org_img.shape) // jnp.array([2, 2, 1]),
+        img = jax.image.resize(org_img, jnp.array(org_img.shape) // jnp.array([2, 2, 1]),
                             method='lanczos5')
         img -= jnp.minimum(0, jnp.min(img))
         img /= jnp.maximum(1, jnp.max(img))
-        crop_keys = random.randint(key=random.PRNGKey(123), shape=[RANDOM_CROP_COUNT],
+        crop_keys = jax.random.randint(key=jax.random.PRNGKey(123), shape=[RANDOM_CROP_COUNT],
                                    minval=0, maxval=1e6)
         for crop_key in crop_keys:
-            crop_img = pix.random_crop(key=random.PRNGKey(crop_key), image=img,
+            crop_img = pix.random_crop(key=jax.random.PRNGKey(crop_key), image=img,
                                        crop_sizes=(TRAIN_PIXELS, TRAIN_PIXELS, 3))
             preprocessed_imgs.append(crop_img)
 
